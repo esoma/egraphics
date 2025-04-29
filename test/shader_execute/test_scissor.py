f96caae8e43fca064a4196f721cc95433a534934
Erik Soma <stillusingirc@gmail.com>
2025-04-28 21:05:28 -0400

    full scissor

--- test/shader_execute/test_scissor.py
+++ test/shader_execute/test_scissor.py
@@ -79,7 +79,7 @@ def test_basic(render_target, pixel):
             (0, 4),
         ),
         {"color": color},
-        scissor=scissor,
+        scissor=IBoundingBox2d(IVector(0), render_target.size),
     )
 
     colors = read_color_from_render_target(
